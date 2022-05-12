16cb3fe67f92e7ad07e1bac8a6649aa8fad6aabf
BuildTools <unconfigured@null.spigotmc.org>
2022-05-12 18:04:02 -0400

    hi

--- FivePD P Duty Plugin/forceextras.cs
+++ FivePD P Duty Plugin/forceextras.cs
@@ -6,6 +6,7 @@ using FivePD.API;
 using FivePD.API.Utils;
 using CitizenFX.Core.Native;
 using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace forceextras
 {
@@ -16,27 +17,66 @@ namespace forceextras
             internal Plugin()
             {
 
-                Tick += EnableExtras;
+                EventHandlers["FivePD::Client::SpawnVehicle"] += new Action<int, int>(EnableExtras); //Natixco
+
             }
 
-            private async Task EnableExtras()
+
+            private void EnableExtras(int playerServerID, int vehicleNetworkID)
             {
+                
+
+                Vehicle pvehicle = Game.PlayerPed.CurrentVehicle;
+                var vehiclename = Game.PlayerPed.CurrentVehicle.DisplayName;
+                var config = API.LoadResourceFile("fivepd", "/config/vehicles.json");
+                var json = JObject.Parse(config);
+                JToken policeVehicles = json["police"];
+                Debug.Write("Vehicle Name: " + vehiclename);
+
+                JToken playerVehicleConfig = policeVehicles.FirstOrDefault(vehicle => vehicle["vehicle"].ToString() == vehiclename);
+
+                JToken extras = playerVehicleConfig["extras"];
+                
+                if (extras != null)
+                {
 
 
 
-                    Vehicle PlayerCar = Game.PlayerPed.VehicleTryingToEnter;
-                    if (PlayerCar.IsDamaged)
-                    { }
-                    else
+                    Debug.Write("\n Extras:" + extras);
+
+                    // First disable any extras
+                    pvehicle.ToggleExtra(1, false);
+                    pvehicle.ToggleExtra(2, false);
+                    pvehicle.ToggleExtra(3, false);
+                    pvehicle.ToggleExtra(4, false);
+                    pvehicle.ToggleExtra(5, false);
+                    pvehicle.ToggleExtra(6, false);
+                    pvehicle.ToggleExtra(7, false);
+                    pvehicle.ToggleExtra(8, false);
+                    pvehicle.ToggleExtra(9, false);
+                    pvehicle.ToggleExtra(10, false);
+                    pvehicle.ToggleExtra(11, false);
+                    pvehicle.ToggleExtra(12, false);
+                    foreach (int extra in extras)
                     {
-                        var VehicleName = PlayerCar.DisplayName;
-                        int num = JToken.op_Explicit(JObject.Parse(CitizenFX.Core.Native.API.LoadResourceFile(CitizenFX.Core.Native.API.GetCurrentResourceName(), "config/forcereg.json"))["ForceDept"]);
-                        await Delay(500);
+                        pvehicle.ToggleExtra(extra, true);
+                        Debug.Write("\nExtra " + extra + " activated.");
                     }
+                }
+                else
+                {
+                    Debug.Write("\n" + vehiclename + " has no extras defined in vehicles.json.");
+                }
+
+
+
+
+
+
+
 
 
             }
         }
     }
-}
 }
