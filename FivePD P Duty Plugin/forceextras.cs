7d841de4cfd85fb10922f4a57aa521e644b8c4d6
BuildTools <unconfigured@null.spigotmc.org>
2022-05-13 17:02:29 -0400

    1.0.0

--- FivePD P Duty Plugin/forceextras.cs
+++ FivePD P Duty Plugin/forceextras.cs
@@ -7,6 +7,7 @@ using FivePD.API.Utils;
 using CitizenFX.Core.Native;
 using Newtonsoft.Json.Linq;
 using System.Linq;
+using Newtonsoft.Json.Schema;
 
 namespace forceextras
 {
@@ -27,55 +28,82 @@ namespace forceextras
                 
 
                 Vehicle pvehicle = Game.PlayerPed.CurrentVehicle;
-                var vehiclename = Game.PlayerPed.CurrentVehicle.DisplayName;
-                var config = API.LoadResourceFile("fivepd", "/config/vehicles.json");
+                var vehiclename = Game.PlayerPed.CurrentVehicle.DisplayName.ToLower();
+                var config = API.LoadResourceFile(API.GetCurrentResourceName(), "/config/vehicles.json");
                 var json = JObject.Parse(config);
                 JToken policeVehicles = json["police"];
-                Debug.Write("Vehicle Name: " + vehiclename);
+                Debug.Write("\nVehicle Name: " + vehiclename);
 
-                JToken playerVehicleConfig = policeVehicles.FirstOrDefault(vehicle => vehicle["vehicle"].ToString() == vehiclename);
-
-                JToken extras = playerVehicleConfig["extras"];
-                
-                if (extras != null)
+                if (policeVehicles.FirstOrDefault(vehicle => vehicle["vehicle"].ToString().ToLower() == vehiclename)["extras"] != null)
                 {
-
-
-
+                JToken playerVehicleConfig = policeVehicles.FirstOrDefault(vehicle => vehicle["vehicle"].ToString().ToLower() == vehiclename);
+                    JToken extras = playerVehicleConfig["extras"];
                     Debug.Write("\n Extras:" + extras);
 
-                    // First disable any extras
-                    pvehicle.ToggleExtra(1, false);
-                    pvehicle.ToggleExtra(2, false);
-                    pvehicle.ToggleExtra(3, false);
-                    pvehicle.ToggleExtra(4, false);
-                    pvehicle.ToggleExtra(5, false);
-                    pvehicle.ToggleExtra(6, false);
-                    pvehicle.ToggleExtra(7, false);
-                    pvehicle.ToggleExtra(8, false);
-                    pvehicle.ToggleExtra(9, false);
-                    pvehicle.ToggleExtra(10, false);
-                    pvehicle.ToggleExtra(11, false);
-                    pvehicle.ToggleExtra(12, false);
-                    foreach (int extra in extras)
+                    if (extras.ToString() == "all")
                     {
-                        pvehicle.ToggleExtra(extra, true);
-                        Debug.Write("\nExtra " + extra + " activated.");
+                        pvehicle.ToggleExtra(1, true);
+                        pvehicle.ToggleExtra(2, true);
+                        pvehicle.ToggleExtra(3, true);
+                        pvehicle.ToggleExtra(4, true);
+                        pvehicle.ToggleExtra(5, true);
+                        pvehicle.ToggleExtra(6, true);
+                        pvehicle.ToggleExtra(7, true);
+                        pvehicle.ToggleExtra(8, true);
+                        pvehicle.ToggleExtra(9, true);
+                        pvehicle.ToggleExtra(10, true);
+                        pvehicle.ToggleExtra(11, true);
+                        pvehicle.ToggleExtra(12, true);
                     }
-                }
-                else
+                    else
+                    if (extras.ToString() == "none")
+                    {
+                        pvehicle.ToggleExtra(1, false);
+                        pvehicle.ToggleExtra(2, false);
+                        pvehicle.ToggleExtra(3, false);
+                        pvehicle.ToggleExtra(4, false);
+                        pvehicle.ToggleExtra(5, false);
+                        pvehicle.ToggleExtra(6, false);
+                        pvehicle.ToggleExtra(7, false);
+                        pvehicle.ToggleExtra(8, false);
+                        pvehicle.ToggleExtra(9, false);
+                        pvehicle.ToggleExtra(10, false);
+                        pvehicle.ToggleExtra(11, false);
+                        pvehicle.ToggleExtra(12, false);
+                    }
+                    else
+                    {
+                        // First disable any extras
+                        pvehicle.ToggleExtra(1, false);
+                        pvehicle.ToggleExtra(2, false);
+                        pvehicle.ToggleExtra(3, false);
+                        pvehicle.ToggleExtra(4, false);
+                        pvehicle.ToggleExtra(5, false);
+                        pvehicle.ToggleExtra(6, false);
+                        pvehicle.ToggleExtra(7, false);
+                        pvehicle.ToggleExtra(8, false);
+                        pvehicle.ToggleExtra(9, false);
+                        pvehicle.ToggleExtra(10, false);
+                        pvehicle.ToggleExtra(11, false);
+                        pvehicle.ToggleExtra(12, false);
+                        foreach (int extra in extras)
+                        {
+                            if (pvehicle.ExtraExists(extra))
+                            {
+                                pvehicle.ToggleExtra(extra, true);
+                                Debug.Write("\nExtra " + extra + " activated.");
+                            } else
+                            {
+                                Debug.Write("\nExtra " + extra + " doesn't exist on vehicle " + vehiclename.ToLower());
+                            }
+                        }
+                    }
+                } else
                 {
                     Debug.Write("\n" + vehiclename + " has no extras defined in vehicles.json.");
+
                 }
-
-
-
-
-
-
-
-
-
+                return;
             }
         }
     }
